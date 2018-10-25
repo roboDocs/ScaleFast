7df11b0c232f712355cb1070c2cbfd3e4e2f98c8
Gustavo Ferreira <gustavo@hipertipo.com>
2018-10-25 14:00:32 -0300

    switching from git submodule to git subtree

---
+++ ScaleFast.roboFontExt/lib/MutatorScale/lib/robofont-test.py
@@ -0,0 +1,21 @@
+from mutatorScale.objects.scaler import MutatorScaleEngine
+
+# To test this, open some interpolatable fonts (if you don’t have any (duh?), use those in the TestFonts folder)
+
+fonts = [font for font in AllFonts() if font.info.familyName != 'Output']
+
+scaler = MutatorScaleEngine(fonts)
+scaler.set({
+    'scale':(0.85, 0.8)
+    })
+
+f = RFont(showInterface=False)
+f.info.familyName = 'Output'
+
+for glyphName in ['A','H','I','O']:
+    stemValues = (100, 70)
+    g = scaler.getScaledGlyph(glyphName, stemValues)
+    f.insertGlyph(g, glyphName)
+
+f.openInterface()
+
